cbaffef55e129e8a8fb32c847022eefcd00c6e82
francisco campos <camqui2011@gmail.com>
2016-05-26 10:19:38 -0430

    mejoras en el start

--- start.php
+++ start.php
@@ -14,6 +14,7 @@
 
 //CONSTANTE DEL LA LIBRERIA
 
+use \Config\Autoload;
 
 //PATH
 define('GETBD', dirname( __FILE__ ));
@@ -21,13 +22,8 @@ define('GETBD', dirname( __FILE__ ));
 define('DS', DIRECTORY_SEPARATOR );
 
 
-/*
-* ARCHIVO A INCLUIR PARA EL INICIO 
-* DE GETBD
-*
-* USADO CON CARGA POR COMPOSER
-*
-*
-*USADO CON CARGA MANUAL
-*/
-require_once (GETBD . DS . 'Config/autocarga.php');
+Autoload::load();
+
+
+//require_once (GETBD . DS . 'Config/autocarga.php');
+//Config\Autoload::load();
